38d6259c5243430f68c53f6e340e77117a46ad4b
Flitzso <106411702+flitzso@users.noreply.github.com>
2023-02-24 15:31:38 -0300

    Add files via upload

---
+++ gerador-senha-csharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string senhaAleatoria = GerarSenhaAleatoria(8);
+        Console.WriteLine("Senha Gerada: " + senhaAleatoria);
+    }
+
+    static string GerarSenhaAleatoria(int comprimento)
+    {
+        const string caracteresPermitidos = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+
+        return new string(Enumerable.Repeat(caracteresPermitidos, comprimento)
+          .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+}
